82d81cdf353856c37cda51c7386687077ec4c123
mavaras <elasgard@hotmail.es>
2019-12-09 21:08:33 -0500

    [#9] show last picture
    - show last picture while finding solution to YouTube links

--- app/screens/Picture.js
+++ app/screens/Picture.js
@@ -10,15 +10,14 @@ import Picture from '../components/PictureComponent';
 import { NASA_API_KEY } from 'react-native-dotenv';
 
 
-export default class PictureScreen extends React.Component {
-  DB = FirebaseDB.instance;
+export default class PictureScreen extends React.Component {  
   constructor(props) {
     super(props);
     this.state = {
       loading: true,
       dataSource: []
     };
-
+    this.db = FirebaseDB.instance;
     /*pictures.on('value', data => {
       //console.log(data.val());
     });*/
@@ -29,24 +28,31 @@ export default class PictureScreen extends React.Component {
     .then(response => response.json())
     .then((responseJson) => {
       this.response = responseJson;
+      if (this.response.url.includes('youtube')) {
+        this.db.pictures.on('value', data => { this.pictures_list = data.val(); });
+        this.pictures_list = Object.values(this.pictures_list);
+        this.response = this.pictures_list[this.pictures_list.length-2];
+      }
+      else {
+        this.db.pictures
+          .orderByChild("title")
+          .equalTo(this.response.title)
+          .once("value")
+          .then(snapshot => {
+            if (!snapshot.val()) {
+              this.DB.pictures.push({
+                title: this.response.title,
+                explanation: this.response.explanation,
+                url: this.response.url,
+                date: this.response.date
+              });
+            }
+          });
+      }
       this.setState({
         loading: false,
         dataSource: responseJson
       });
-      this.DB.pictures
-        .orderByChild("title")
-        .equalTo(this.response.title)
-        .once("value")
-        .then(snapshot => {
-          if (!snapshot.val()) {
-            this.DB.pictures.push({
-              title: this.response.title,
-              explanation: this.response.explanation,
-              url: this.response.url,
-              date: this.response.date
-            });
-          }
-        });
     })
     .catch(error => console.log(error))
   }
@@ -75,7 +81,7 @@ export default class PictureScreen extends React.Component {
           </View>
         );
       }
-      else {console.log(this.response);
+      else {
         return(
           <ScrollView contentContainerStyle={{}}>
             <Picture attrs={this.response}/>
