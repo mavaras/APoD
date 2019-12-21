cfb97ade6e74251f64d32f612a226d9ac0ff8049
Mario Varas <elasgard@hotmail.es>
2019-12-21 08:01:02 -0500

    [#2] download picture to gallery

--- app/components/PictureComponent.js
+++ app/components/PictureComponent.js
@@ -3,24 +3,47 @@ import {
   Image,
   Text,
   TouchableHighlight,
-  View
+  View,
+  PermissionsAndroid
 } from 'react-native';
 import Modal from "react-native-modal";
 import Icon from "react-native-vector-icons/FontAwesome";
 import RNFetchBlob from 'rn-fetch-blob';
 
 
+export async function request_storage_runtime_permission() {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+      {
+        'title': 'ReactNativeCode Storage Permission',
+        'message': 'ReactNativeCode App needs access to your storage to download Photos.'
+      }
+    )
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log("Storage Permission Granted");
+    }
+    else {
+      console.log("Storage Permission Not Granted");
+    }
+  } 
+  catch (err) {
+    console.log('Some error while granting storage access: ' + err);
+  }
+}
+
 export default class Picture extends React.Component {
   constructor() {
     super();
     this.state = {
-      popoverIsVisible: false,
+      isModalOpen: false,
       buttonRect: {x: 0, y: 0, width: 0, height: 0}
     };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     this.refs = React.createRef();
+    await request_storage_runtime_permission();
   }
 
   getInitialState() {
@@ -32,61 +55,77 @@ export default class Picture extends React.Component {
 
   download() {
     const { config, fs } = RNFetchBlob;
-    let PictureDir = fs.dirs.PictureDir;
     const date = new Date();
     let options = {
       fileCache: true,
       addAndroidDownloads : {
+        useDownloadManager: true,
         notification : true,
-        title: 'title',
-        path:  fs.dirs.DownloadDir + "/algo.jpg", //"/APoD_"+Math.floor(date.getTime() + date.getSeconds() / 2),
+        title: 'APoD picture download',
+        path:  fs.dirs.PictureDir + '/APoD_' + Math.floor(date.getTime() + date.getSeconds() / 2) + '.png',
         fileCache: true,
-        description : 'Downloading image.',
+        description : 'Downloading picture',
         mediaScannable: true,
       }
     };
-    config(options).fetch('GET', 'https://blog.jscrambler.com/content/images/2018/12/jscrambler-blog-integrating-firebase-with-react-native-firebase-new-project.png')
+    config(options).fetch('GET', this.props.attrs.url)
     .then((res) => {
-      console.log("success "+res.path());
+      console.log('Success downloading photo to path: ' + res.path());
+      this.setState({
+        isModalOpen: false
+      });
+    })
+    .catch((err) => {
+      console.log('Error occurred when trying to download picture: ' + err);
     });
   }
 
   showPopover() {
     this.image_ref.measure((ox, oy, width, height, px, py) => {
       this.setState({
-        popoverIsVisible: true,
+        isModalOpen: true,
         buttonRect: {x: px, y: py, width: width, height: height}
       });
     });
   }
   
-  closePopover() {
-    this.setState({ popoverIsVisible: false });
+  closeModal() {
+    this.setState({ isModalOpen: false });
   }
 
   render() {
     return (
       <View style={{flex: 1}}>
-        <TouchableHighlight ref={(r) => {this.image_ref = r}} onPress={this.showPopover.bind(this)} style={{'alignItems': 'center'}}>
+        <TouchableHighlight
+          ref={(r) => {this.image_ref = r}}
+          onPress={this.showPopover.bind(this)}
+          style={{'alignItems': 'center'}}
+        >
           <Image
             style={{width: '90%', height: 300, marginTop: 20}}
             source={{uri: this.props.attrs.url}}
           />
         </TouchableHighlight>
         <Modal
-          isVisible={this.state.popoverIsVisible}
+          isVisible={this.state.isModalOpen}
           style={{flex: 1, 'flexDirection': 'row', alignItems: 'center', justifyContent: 'center'}}
-          onBackdropPress={this.closePopover.bind(this)}
+          onBackdropPress={this.closeModal.bind(this)}
         >
-          <View style={{flex: 1, height: 120, backgroundColor: 'white', 
-                        'flexDirection': 'column', alignItems: 'center', justifyContent: 'center',
-                        'borderRadius': 5}}>
+          <View 
+            style={{flex: 1, height: 120, backgroundColor: 'white', 
+                    'flexDirection': 'column', alignItems: 'center', justifyContent: 'center',
+                    'borderRadius': 5}}
+          >
             <View style={{'marginBottom': 30}}>
               <Text style={{fontSize: 19}}>Modal content</Text>
             </View>
             <View style={{'flexDirection': 'row', justifyContent: 'space-between'}}>
               <View style={{'paddingRight': 10, width: '40%'}}>
-                <Icon.Button name="download" backgroundColor="#5b84c2" onPress={() => {this.download()}}>
+                <Icon.Button
+                  name="download"
+                  backgroundColor="#5b84c2"
+                  onPress={() => { this.download(); }}
+                >
                   <Text style={{'color': 'white'}}>Download</Text>
                 </Icon.Button>
               </View>
