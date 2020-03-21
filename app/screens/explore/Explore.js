a88b26056d3fac45967a38125786a1d8706d3d55
Mario Varas <elasgard@hotmail.es>
2020-03-21 17:16:59 -0500

    (feat) [#15] real dynamic loading
    - bottom spinner
    - async Firebase data get
    - spinner behaviour will be better
    - it will be necessary to think about animation while displaying images

--- app/screens/explore/Explore.js
+++ app/screens/explore/Explore.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {
   FlatList,
   SafeAreaView,
+  RefreshControl
 } from 'react-native';
 import FirebaseDB from '../../config';
 import styles from './style';
-import LoadingScreen from '../loading/LoadingScreen';
 import PictureSmall from '../../components/Picture/PictureComponentSmall';
 
 
@@ -13,14 +13,15 @@ export default class ExploreScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: true,
+      loading: false,
+      refreshing: false,
       loadMore: false,
       page: 1,
       pictures: []
     }
     this.DB = FirebaseDB.instance;
     this.n_pictures = 0;
-    this.pictures_limit = 6;
+    this.pictures_limit = 8;
     this.pictures = [[], []];
   }
 
@@ -28,62 +29,60 @@ export default class ExploreScreen extends React.Component {
     this.loadMoreData();
   }
 
-  loadMoreData() {
+  async loadMoreData() {
     const { loadMore } = this.state;
     if (loadMore ||
       this.state.pictures.length == this.pictures_list) {
       return;
     }
     this.setState({
-      loading: false,
-      loadMore: true
-    });
-    this.DB.pictures
-      .on('value', data => { 
-        this.pictures_list = data.val();
-        this.pictures_list = Object.values(this.pictures_list);
-        this.pictures_list = this.pictures_list.filter((e) => {
-          if(!['youtube', 'vimeo'].some(aux => e.url.split(/[/.]/).includes(aux))) {
-            return e;
-          }
+      loading: true,
+      refreshing: true,
+    }, async () => {
+      await this.DB.pictures
+        .once('value', data => { 
+          this.pictures_list = data.val();
+          this.pictures_list = Object.values(this.pictures_list);
+          this.pictures_list = this.pictures_list.filter((e) => {
+            if(!['youtube', 'vimeo'].some(aux => e.url.split(/[/.]/).includes(aux))) {
+              return e;
+            }
+          });
+          this.n_pictures = this.pictures_list.length;
+          this.setState({
+            pictures: [...this.pictures_list.splice(0, this.pictures_limit * this.state.page)].reverse(),
+            page: this.state.page + 1,
+            loading: false,
+            refreshing: false,
+          });
         });
-        this.pictures_list.splice(-1, 1).sort(() => Math.random() - 0.5);
-        this.n_pictures = this.pictures_list.length;
-        this.setState({
-          pictures: [...this.pictures_list.splice(0, this.pictures_limit * this.state.page)],
-          page: this.state.page + 1
-        });
-    });
-    this.setState({
-      loading: false,
-      loadMore: false,
     });
   }
 
   render() {
-    if (!this.state.loading && !this.state.loadMore) {
-      return (
-        <SafeAreaView>
-          <FlatList
-            style={styles.flatList}              
-            data={this.state.pictures}
-            renderItem={({item, index}) => (
-              <PictureSmall
-                picture={item}
-                index={index}
-                navigation={this.props.navigation} />
-            )}
-            keyExtractor={item => item.title.toString()}
-            onEndReached={() => { this.loadMoreData(); }}
-            onEndReachedThreshold={0.5}
-            numColumns={2}
-            initialNumToRender={6}
-          />
-        </SafeAreaView>
-      );
-    }
     return (
-      <LoadingScreen/>
+      <SafeAreaView style={{ flex: 1 }}>
+        <FlatList inverted
+          style={styles.flatList}
+          data={this.state.pictures}
+          extraData={this.state}
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this.loadMoreData.bind(this)}
+            />
+          }
+          renderItem={({item, index}) => (
+            <PictureSmall
+              picture={item}
+              index={index}
+              navigation={this.props.navigation}
+            />
+          )}
+          keyExtractor={item => item.title.toString()}
+          numColumns={2}
+        />
+      </SafeAreaView>
     );
   }
 }
