1f0fb1d7a9b065cd916b9fe704ec222a61493b40
Mario Varas <elasgard@hotmail.es>
2020-04-09 21:49:29 -0500

    (style) remove unused code

--- app/screens/explore/style.js
+++ app/screens/explore/style.js
@@ -2,16 +2,9 @@ import { StyleSheet } from 'react-native';
 
 
 export default StyleSheet.create({
-  exploreTitle: {
-    marginTop: 25,
-    marginBottom: 15,
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: 18
-  },
   flatList: {
-    marginTop: 10,
-    marginBottom: 8
+    marginTop: 0,
+    marginBottom: 0
   },
   safeAreaView: {
     flex: 1
