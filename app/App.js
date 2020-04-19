cf39c6df5d876b25bfb3011d54b5319b96ff8d69
Mario Varas <elasgard@hotmail.es>
2020-04-19 18:55:00 -0500

    (style) linting

--- app/App.js
+++ app/App.js
@@ -1,11 +1,11 @@
 import 'react-native-gesture-handler';
-import FontAwesome from "react-native-vector-icons/FontAwesome5";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesome from 'react-native-vector-icons/FontAwesome5';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
 import PictureScreen from './screens/picture/Picture';
 import ExploreScreen from './screens/explore/Explore';
-import React from 'react';
 
 
 console.disableYellowBox = true;
@@ -15,25 +15,25 @@ const Tabs = createBottomTabNavigator();
 
 function BottomTabNavigator() {
   return (
-    <Tabs.Navigator 
+    <Tabs.Navigator
       tabBarOptions={{
         activeTintColor: '#007AFF'
       }}
     >
       <Tabs.Screen
-        name='Daily Picture'
+        name="Daily Picture"
         component={PictureScreen}
         options={{
           attrs: undefined,
-          tabBarIcon: ({ focused }) => <FontAwesome name='meteor' size={26} color={focused ? '#007AFF' : 'gray'} />
+          tabBarIcon: ({ focused }) => <FontAwesome name="meteor" size={26} color={focused ? '#007AFF' : 'gray'} />
         }}
       />
       <Tabs.Screen
-        name='Explore'
+        name="Explore"
         component={ExploreScreen}
         options={{
           navigation: undefined,
-          tabBarIcon: ({ focused }) => <FontAwesome name='rocket' size={26} color={focused ? '#007AFF' : 'gray'} />
+          tabBarIcon: ({ focused }) => <FontAwesome name="rocket" size={26} color={focused ? '#007AFF' : 'gray'} />
         }}
       />
     </Tabs.Navigator>
@@ -45,12 +45,12 @@ function StackNavigator() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name='Back'
+          name="Back"
           component={BottomTabNavigator}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name='Explore Picture'
+          name="Explore Picture"
           component={PictureScreen}
           options={{ headerTitle: null }}
         />
