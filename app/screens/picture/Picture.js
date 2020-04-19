9fbf85d7d9bfd34ff804f3b2e93fc98964311ae3
Mario Varas <elasgard@hotmail.es>
2020-04-19 14:14:54 -0500

    (style) linting

--- app/screens/picture/Picture.js
+++ app/screens/picture/Picture.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { ScrollView } from 'react-native';
-import FirebaseDB from '../../config';
 import { NASA_API_KEY } from 'react-native-dotenv';
+import FirebaseDB from '../../config';
 import Picture from '../../components/Picture/PictureComponent';
 import LoadingScreen from '../loading/LoadingScreen';
 
 
 function PictureScreen({ route }) {
-  DB = FirebaseDB.instance;
+  const DB = FirebaseDB.instance; // eslint-disable-line no-undef
   const [loading, setLoading] = useState(true);
-  const [dataSource, setDataSource] = useState([]);
-  let [response, setResponse] = useState({});
+  const [, setDataSource] = useState([]);
+  const [response, setResponse] = useState({});
 
   function fetchData() {
     if (!route.params?.attrs) {
@@ -19,15 +19,17 @@ function PictureScreen({ route }) {
       setTimeout(async () => {
         await DB.pictures
           .limitToLast(1)
-          .once('value', data => { 
+          .once('value', (data) => {
+            // eslint-disable-next-line prefer-destructuring
             last_picture = Object.values(data.val())[0];
             let today = new Date();
+            // eslint-disable-next-line prefer-template
             today = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + today.getDate();
-            must_query = today != last_picture['date'];
+            must_query = today !== last_picture.date;
           });
         if (must_query) {
-          fetch('https://api.nasa.gov/planetary/apod?api_key=' + NASA_API_KEY)
-            .then(response => response.json())
+          fetch(`https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`)
+            .then((_response) => _response.json())
             .then((responseJson) => {
               if (!('error' in responseJson)) {
                 setResponse(responseJson);
@@ -35,7 +37,7 @@ function PictureScreen({ route }) {
                   .orderByChild('title')
                   .equalTo(responseJson.title)
                   .once('value')
-                  .then(snapshot => {
+                  .then((snapshot) => {
                     if (!snapshot.val()) {
                       DB.pictures.push({
                         title: response.title,
@@ -49,10 +51,10 @@ function PictureScreen({ route }) {
                   });
               }
               else {
-                throw 'error in response';
+                throw new Error('error in response');
               }
             })
-            .catch(error => console.log(error))
+            .catch((error) => console.log(error));
         }
         else {
           setResponse(last_picture);
@@ -72,16 +74,14 @@ function PictureScreen({ route }) {
 
   if (loading) {
     return (
-      <LoadingScreen/>
+      <LoadingScreen />
     );
   }
-  else {
-    return (
-      <ScrollView contentContainerStyle={{ backgroundColor: 'white', height: '100%' }}>
-        <Picture attrs={response}/>
-      </ScrollView>
-    );
-  }
+  return (
+    <ScrollView contentContainerStyle={{ backgroundColor: 'white', height: '100%' }}>
+      <Picture attrs={response} />
+    </ScrollView>
+  );
 };
 
 export default PictureScreen;
