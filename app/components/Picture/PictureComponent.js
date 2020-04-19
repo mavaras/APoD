49da7fa6fb7e848d40e1a14954847cac982f791d
Mario Varas <elasgard@hotmail.es>
2020-04-19 14:35:17 -0500

    (style) linting

--- app/components/Picture/PictureComponent.js
+++ app/components/Picture/PictureComponent.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from 'react';
 import {
   Text,
   TouchableHighlight,
-  View, SafeAreaView,
-  PermissionsAndroid,
+  View, SafeAreaView, Platform
 } from 'react-native';
 import Dialog, {
   DialogButton,
   DialogContent,
   DialogFooter,
   DialogTitle,
-  SlideAnimation } from 'react-native-popup-dialog';
+  SlideAnimation
+} from 'react-native-popup-dialog';
 import FastImage from 'react-native-fast-image';
 import CameraRoll from '@react-native-community/cameraroll';
 import Share from 'react-native-share';
@@ -18,9 +18,9 @@ import ImgToBase64 from 'react-native-image-base64';
 import Modal from 'react-native-modal';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import RNFetchBlob from 'rn-fetch-blob';
+import { ScrollView } from 'react-native-gesture-handler';
 import styles from './style';
 import Video from '../Video/VideoComponent';
-import { ScrollView } from 'react-native-gesture-handler';
 import { request_storage_runtime_permission_android } from '../../utils';
 
 
@@ -28,33 +28,35 @@ function Picture({ attrs }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [downloading, setDownloading] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [buttonRect, setButtonRect] = useState({ x: 0, y: 0, width: 0, height: 0 });
-  let image_ref = undefined;
+  const [, setButtonRect] = useState({
+    x: 0, y: 0, width: 0, height: 0
+  });
+  let image_ref;
 
   useEffect(() => {
     this.refs = React.createRef();
-    if (Platform.OS == 'android') {
+    if (Platform.OS === 'android') {
       request_storage_runtime_permission_android();
     }
   });
 
   const download = () => {
     const { config, fs } = RNFetchBlob;
-    let picture_dir = Platform.OS === 'ios' ? fs.dirs.DocumentDir : fs.dirs.PictureDir;
-    const picture_path = picture_dir + '/APoD/APoD_' + attrs.title + '.png';
+    const picture_dir = Platform.OS === 'ios' ? fs.dirs.DocumentDir : fs.dirs.PictureDir;
+    const picture_path = `${picture_dir}/APoD/APoD_${attrs.title}.png`;
 
     setDownloading(true);
 
-    if (Platform.OS == 'android') {
-      let options = {
+    if (Platform.OS === 'android') {
+      const options = {
         fileCache: true,
-        addAndroidDownloads : {
+        addAndroidDownloads: {
           useDownloadManager: true,
-          notification : true,
+          notification: true,
           title: 'APoD picture download',
           path: picture_path,
           fileCache: true,
-          description : 'Downloading picture',
+          description: 'Downloading picture',
           mediaScannable: true,
         }
       };
@@ -67,10 +69,9 @@ function Picture({ attrs }) {
         .catch((err) => {
           console.log('Error occurred when trying to download picture: ' + err);
         });
-    }
-    else {
+    } else {
       CameraRoll.saveToCameraRoll(attrs.url)
-        .then((res) => {
+        .then(() => {
           console.log('Success downloading photo to camera roll');
           setDownloading(false);
           setIsModalOpen(false);
@@ -80,50 +81,52 @@ function Picture({ attrs }) {
           console.log('Error occurred when trying to download picture: ' + err);
         });
     }
-  }
+  };
 
   const share = () => {
     ImgToBase64.getBase64String(attrs.url)
-      .then(base64_url => {
+      .then((base64_url) => {
         const options = {
-          url: 'data:image/png;base64,' + base64_url
+          url: `data:image/png;base64,${base64_url}`
         };
         Share.open(options)
           .catch((err) => {
             console.log(err);
           });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
-  }
+  };
 
   const showPopover = () => {
     image_ref.measure((width, height, x, y) => {
       setIsModalOpen(true);
-      setButtonRect({x: x, y: y, width: width, height: height});
+      setButtonRect({
+        x, y, width, height
+      });
     });
-  }
-  
+  };
+ 
   const closeModal = () => {
     setIsModalOpen(false);
-  }
+  };
 
   return (
     <SafeAreaView>
       <Dialog
         dialogAnimation={new SlideAnimation({
-          slideFrom: "bottom",
+          slideFrom: 'bottom',
         })}
         dialogTitle={<DialogTitle title="APoD" />}
-        footer={
+        footer={(
           <DialogFooter>
             <DialogButton
               text="OK"
               onPress={() => { setShowAlert(false); }}
             />
           </DialogFooter>
-        }
+        )}
         visible={showAlert}
         onTouchOutside={() => {
           setShowAlert(false);
@@ -134,20 +137,21 @@ function Picture({ attrs }) {
         </DialogContent>
       </Dialog>
       <ScrollView>
-      {!['youtube', 'vimeo'].some(aux => attrs.url.split(/[/.]/).includes(aux)) ?
-        <TouchableHighlight
-          ref={(r) => {image_ref = r}}
-          onPress={showPopover.bind(this)}
-          underlayColor="none"
-          style={styles.touchableHighlight}
-        >
-          <FastImage
-            style={styles.image}
-            source={{uri: attrs.url}}
-          />
-        </TouchableHighlight> :
-        <Video url={attrs.url}/>
-      }
+        {!['youtube', 'vimeo'].some((aux) => attrs.url.split(/[/.]/).includes(aux))
+          ? (
+            <TouchableHighlight
+              ref={(r) => { image_ref = r; }}
+              onPress={showPopover.bind(this)}
+              underlayColor="none"
+              style={styles.touchableHighlight}
+            >
+              <FastImage
+                style={styles.image}
+                source={{ uri: attrs.url }}
+              />
+            </TouchableHighlight>
+          )
+          : (<Video url={attrs.url} />)}
         <Modal
           isVisible={isModalOpen}
           style={styles.modal}
@@ -183,22 +187,25 @@ function Picture({ attrs }) {
             </View>
           </View>
         </Modal>
-        { attrs.date !== undefined ?
-        <View>
-          <View style={styles.viewPictureText}>
-            <Text style={styles.textPictureTitle}>
-              {attrs.title}
-            </Text>
-            <Text style={styles.textPictureExplanation}>
-              {attrs.explanation}
-            </Text>
-          </View>
-          <View>
-            <Text style={styles.viewPictureDate}>
-              {attrs.date}
-            </Text>
-          </View>
-        </View> : undefined }
+        { attrs.date !== undefined
+          ? (
+            <View>
+              <View style={styles.viewPictureText}>
+                <Text style={styles.textPictureTitle}>
+                  {attrs.title}
+                </Text>
+                <Text style={styles.textPictureExplanation}>
+                  {attrs.explanation}
+                </Text>
+              </View>
+              <View>
+                <Text style={styles.viewPictureDate}>
+                  {attrs.date}
+                </Text>
+              </View>
+            </View>
+          )
+          : undefined }
       </ScrollView>
     </SafeAreaView>
   );
--- app/components/Picture/PictureComponentSmall.js
+++ app/components/Picture/PictureComponentSmall.js
@@ -7,27 +7,29 @@ import styles from './style';
 
 
 function PictureComponentSmall({ picture, index, navigation }) {
-  function itemStyle(index) {
-    return (
-      [
-        {flex: 1, marginTop: 0, marginBottom: 6, height: 200 },
-        index % 2 == 0 ? { marginLeft: 6, marginRight: 3 } :
-                          { marginRight: 6, marginLeft: 3 }
-      ]
-    );
-  };
+  function itemStyle(indx) {
+    return ([
+      {
+        flex: 1, marginTop: 0, marginBottom: 6, height: 200
+      },
+      indx % 2 === 0 ? { marginLeft: 6, marginRight: 3 }
+        : { marginRight: 6, marginLeft: 3 }
+    ]);
+  }
 
   return (
     <TouchableHighlight
       style={itemStyle(index)}
       onPress={() => {
-        navigation.navigate('Explore Picture', { attrs: picture })
-      }}>
+        navigation.navigate('Explore Picture', { attrs: picture });
+      }}
+    >
       <FastImage
         style={styles.imageSmall}
-        source={{uri: picture.url}}
+        source={{ uri: picture.url }}
       />
-    </TouchableHighlight>);
+    </TouchableHighlight>
+  );
 }
 
 export default PictureComponentSmall;
