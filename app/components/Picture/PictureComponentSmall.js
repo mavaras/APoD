f07b10c2a45df036a87b6c7f8a449604bc0a34c5
Mario Varas <elasgard@hotmail.es>
2020-04-25 17:00:56 -0500

    (feat) [#15] new sizing algorithm
    - it allows to have more than 2 cols and style pics correctly

--- app/components/Picture/PictureComponentSmall.js
+++ app/components/Picture/PictureComponentSmall.js
@@ -6,14 +6,20 @@ import FastImage from 'react-native-fast-image';
 import styles from './style';
 
 
-function PictureComponentSmall({ picture, index, navigation }) {
+function PictureComponentSmall({
+  picture, index, cols, navigation
+}) {
   function itemStyle(indx) {
+    // eslint-disable-next-line no-nested-ternary
+    const pos = indx % cols === 0 ? 0 : (indx + 1) % cols === 0 ? 1 : 2;
+    const img_height = [400, 200, 150][cols - 1];
     return ([
       {
-        flex: 1, marginTop: 0, marginBottom: 6, height: 200
+        flex: 1, marginTop: 0, marginBottom: 6, height: img_height,
       },
-      indx % 2 === 0 ? { marginLeft: 6, marginRight: 3 }
-        : { marginRight: 6, marginLeft: 3 }
+      // eslint-disable-next-line no-nested-ternary
+      pos === 0 ? { marginLeft: 6, marginRight: 3 }
+        : pos === 1 ? { marginRight: 6, marginLeft: 3 } : { marginRight: 3, marginLeft: 3 }
     ]);
   }
 
