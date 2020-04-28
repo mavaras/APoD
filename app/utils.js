94f2eb2ce5f33206510b60a94ac7a048ef63d1e2
Mario Varas <elasgard@hotmail.es>
2020-04-27 19:53:53 -0500

    (refac) utils refac

--- app/utils.js
+++ app/utils.js
@@ -2,36 +2,36 @@ import { PermissionsAndroid } from 'react-native';
 
 
 export function shuffle_array(array) {
-    var aux, random_index, current_index = array.length;
-    while (current_index != 0) {
-        random_index = Math.floor(Math.random() * current_index);
-        current_index -= 1;
-        aux = array[current_index];
-        array[current_index] = array[random_index];
-        array[random_index] = aux;
-    }
+  let aux;
+  let random_index;
+  let current_index = array.length;
+  while (current_index !== 0) {
+    random_index = Math.floor(Math.random() * current_index);
+    current_index -= 1;
+    aux = array[current_index];
+    array[current_index] = array[random_index];
+    array[random_index] = aux;
+  }
 
   return array;
 }
 
 
 export async function request_storage_runtime_permission_android() {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-        {
-          'title': 'ReactNativeCode Storage Permission',
-          'message': 'ReactNativeCode App needs access to your storage to download Photos.'
-        }
-      )
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('Storage Permission Granted');
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+      {
+        title: 'ReactNativeCode Storage Permission',
+        message: 'ReactNativeCode App needs access to your storage to download Photos.'
       }
-      else {
-        console.log('Storage Permission Not Granted');
-      }
-    } 
-    catch (err) {
-      console.log('Some error while granting storage access: ' + err);
+    )
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('Storage Permission Granted');
+    } else {
+      console.log('Storage Permission Not Granted');
     }
+  } catch (err) {
+    console.log(`Some error while granting storage access: ${err}`);
   }
+}
