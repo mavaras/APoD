1c0179fdb00cd7182842bcb4560ae799fd02da07
Mario Varas <elasgard@hotmail.es>
2020-06-05 22:08:02 -0500

    (style) [#26] waiting screen themed

--- app/screens/loading/WaitingScreen.tsx
+++ app/screens/loading/WaitingScreen.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { Text, SafeAreaView, View } from 'react-native';
 import Animation from 'lottie-react-native';
+import { useTheme } from '../../themes';
 
 
 function WaitingScreen() {
+  const theme = useTheme();
   let animation = require('../../res/animations/planet.json');
 
   useEffect(() => {
@@ -11,7 +13,7 @@ function WaitingScreen() {
   }, []);
 
   return (
-    <SafeAreaView style={{ backgroundColor: 'white', height: '100%' }}>
+    <SafeAreaView style={{ backgroundColor: theme.getColors().bgColor, height: '100%' }}>
       <View style={{ marginTop: '55%', justifyContent: 'center', alignItems: 'center' }}>
         <Animation
           ref={(animationRef) => { animation = animationRef; }}
@@ -20,7 +22,7 @@ function WaitingScreen() {
         />
       </View>
       <View>
-        <Text style={{ fontSize: 21, textAlign: 'center' }}>
+        <Text style={{ color: theme.getColors().fontColor, fontSize: 21, textAlign: 'center' }}>
           Today&apos;s APoD will appear soon.
           {'\n'}
           Stay tuned!
