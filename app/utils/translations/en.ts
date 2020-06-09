33236ab0171b494d97910d9780040bc9c48b49ac
Mario Varas <elasgard@hotmail.es>
2020-06-09 09:50:16 -0500

    (feat) translations
    - utils folder

---
+++ app/utils/translations/en.ts
@@ -0,0 +1,12 @@
+import { ResourceLanguage } from 'i18next';
+
+
+export const enTranslations: ResourceLanguage = {
+  settings: {
+    links: {
+      buyMeACoffee: 'https://www.buymeacoffee.com/mavaras',
+    },
+  },
+};
+
+//export default enTranslations;
