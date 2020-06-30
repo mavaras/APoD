a141dcc77d0dcf323d51ad8187a8978e9c589f40
Mario Varas <elasgard@hotmail.es>
2020-06-29 20:58:18 -0500

    (feat) [#28] apollo client & graphql support

--- index.js
+++ index.js
@@ -3,10 +3,19 @@
  */
 import 'react-native-gesture-handler';
 
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
 import { AppRegistry } from 'react-native';
 
 import { name as appName } from './app.json';
+import apolloClient from './app/apollo';
 import StackNavigator from './app/App';
 
 
-AppRegistry.registerComponent(appName, () => StackNavigator);
+const Root = () => (
+  <ApolloProvider client={apolloClient}>
+    <StackNavigator />
+  </ApolloProvider>
+);
+
+AppRegistry.registerComponent(appName, () => Root);
