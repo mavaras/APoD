21944a3502d8fdd0062cc52589307d9a2893628c
Mario Varas <elasgard@hotmail.es>
2020-07-02 17:02:39 -0500

    Revert "super refac for include backend in here"
    This reverts commit 002c765f563b61001b57ed023d99185075b3268d.

--- app/ios/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  APoD
-//
-//  Created by Mario Varas on 2/1/20.
-//  Copyright © 2020 Facebook. All rights reserved.
-//
-
-import Foundation
--- app/test.py
+++
@@ -1,4 +0,0 @@
-def joder():
-    print("hostias")
-
-joder()
