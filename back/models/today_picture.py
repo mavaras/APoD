f3550da55746e96f8dcc4356b4da8f05d465fd80
Mario Varas <elasgard@hotmail.es>
2020-07-04 12:34:48 -0500

    feat(back): some model changes
    - Similars as picture attribute
    - description changed to explanation, following db model
    - Some packaging

--- back/models/today_picture.py
+++ back/models/today_picture.py
@@ -1,24 +1,28 @@
-from graphene import ObjectType, String
+from graphene import Field, List, ObjectType, String
 
 
 class TodayPicture(ObjectType):
     url = String()
     title = String()
-    description = String()
+    explanation = String()
     author = String()
     date = String()
+    similars = List(String)
 
     def resolve_url(self, info):
         return self.url
     
     def resolve_title(self, info):
         return self.title
-    
-    def resolve_description(self, info):
-        return self.description
+
+    def resolve_explanation(self, info):
+        return self.explanation
 
     def resolve_author(self, info):
         return self.author
 
     def resolve_date(self, info):
         return self.date
+
+    def resolve_similars(self, info):
+        return self.similars
